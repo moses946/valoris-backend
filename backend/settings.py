fba685a0e8e67a781ffe24c54d3bd65883f2bfd3
Moses Mwai <mosesmwaiw@gmail.com>
2024-10-30 01:21:52 +0300

    Change From Email to info@byteflow.co.ke

--- backend/settings.py
+++ backend/settings.py
@@ -117,12 +117,13 @@ USE_I18N = True
 USE_TZ = True
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_FROM = 'happylandbestie@gmail.com'
-EMAIL_HOST_USER = 'happylandbestie@gmail.com'
-EMAIL_HOST_PASSWORD = 'dwjq dkvr wcof oxyu'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.zoho.com'
+EMAIL_PORT = 465
+EMAIL_USE_SSL = True 
+EMAIL_HOST_USER = 'info@byteflow.co.ke'  
+EMAIL_HOST_PASSWORD = 'Mbokavulai197'    
+DEFAULT_FROM_EMAIL = 'info@byteflow.co.ke'
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
